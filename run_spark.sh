b1251fc74002d916f0bced5a4e18d9f1aab69df1
arnold-jr <j.arnold.111@gmail.com>
2017-05-18 14:48:41 -0500

    Add option for running on bigdata

--- run_spark.sh
+++ run_spark.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 0 ]; then
-  echo $0: "usage: ./run_spark.sh"
-  exit 1
-fi
-
+case "$#" in
+    (0)
+        FLAG=local
+        ;;
+    (1)
+        FLAG=$1
+        ;;
+    (*)
+        echo $0: "usage: ./run_spark.sh [local|bigdata]"
+        exit 1
+        ;;
+esac
 
 echo Reading input from $input
 
 
-FLAG=0
 case ${FLAG} in
-    (0)
+    (local)
         # Run application locally
         INPUT="../spark-sem-collate/data.jl"
         MASTER='local[*]'
+        ;;
+    (bigdata)
+        # Run application locally
+        INPUT="hdfs:///user/arnoldjr/data.jl"
+        MASTER='yarn'
         ;;
     (*)
         echo "Invalid FLAG option [$FLAG]"
